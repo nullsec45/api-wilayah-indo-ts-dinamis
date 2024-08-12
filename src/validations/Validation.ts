b616f63f0cd18cb23470ac3c22be497a0a1a8e48
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-08-12 23:15:01 +0700

    feat : create class Validation for Joi validate.

---
+++ src/validations/Validation.ts
@@ -0,0 +1,20 @@
+import ResponseError from "../errors/ResponseError";
+import { Request } from "express";
+import Joi, { Schema } from 'joi';
+
+class Validation{
+   
+    static validate<T extends Schema>(schema:T,request:Request){
+        const result = schema.validate(request, {
+            abortEarly: false,
+            allowUnknown: false,
+        });
+
+        if (result.error) {
+            throw new ResponseError(400, result.error.message);
+        } else {
+            return result.value;
+        }
+    }
+
+}
