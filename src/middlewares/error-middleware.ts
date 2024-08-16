5bd6a4c46fabb1ef27acaba1c9ffc06a178f1233
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-08-16 23:33:28 +0700

    feat : new error middleware

---
+++ src/middlewares/error-middleware.ts
@@ -0,0 +1,16 @@
+import ResponseError from "../errors/ResponseError";
+import { Request, Response, NextFunction } from "express";
+
+const errorMiddleware = async (err: any, req: Request, res: Response, next: NextFunction) => {
+    if (err instanceof ResponseError) {
+        res.status(err.status).json({
+            errors: err.message,
+        });
+    } else {
+        res.status(500).json({
+            errors: err.message || "Internal Server Error",
+        }).end();
+    }
+};
+
+export { errorMiddleware };
