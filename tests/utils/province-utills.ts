5b9c2ecc52ca59041d1eb199d5226f1d6d1db177
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-08-24 23:34:52 +0700

    feat : create province utils

--- tests/utils/province-utills.ts
+++ tests/utils/province-utills.ts
@@ -5,6 +5,16 @@ const removeTestProvince=async () => {
     await prismaClient.$executeRaw`ALTER TABLE provinces AUTO_INCREMENT = 1`;
 }
 
+const createTestProvince=async () => {
+   let province= await prismaClient.province.create({
+        data:{
+            name:"Province Test"
+        }
+    })
+
+    return province;
+}
 export {
-    removeTestProvince
+    removeTestProvince,
+    createTestProvince
 }
