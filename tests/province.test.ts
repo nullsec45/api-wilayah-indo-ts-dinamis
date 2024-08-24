7c170473b9e6939f32402bd45e15c3f7cc9eaa28
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-08-24 23:35:30 +0700

    test : get province

--- tests/province.test.ts
+++ tests/province.test.ts
@@ -1,10 +1,10 @@
 import { server } from "../src/applications/server";
 import supertest from "supertest";
 import { logger } from "../src/applications/logging";
-import {removeTestProvince} from "./utils/province-utills";
+import {removeTestProvince, createTestProvince} from "./utils/province-utills";
 
 
-describe("POST /api/v1/provinces", function(){
+describe.skip("POST /api/v1/provinces", function(){
     afterEach(async () => {
        await  removeTestProvince();
     });
@@ -18,5 +18,27 @@ describe("POST /api/v1/provinces", function(){
 
         expect(result.status).toBe(200);
         expect(result.body.data.name).toBe("DKI Jakarta");
+    });
+});
+
+describe("GET /api/v1/provinces/:id", function(){
+    let province:any=null;
+
+    beforeEach(async() => {
+        province=await createTestProvince();
+    });
+
+    afterEach(async () => {
+       await  removeTestProvince();
+    });
+
+    it("should can get province", async() => {
+        const result=await supertest(server).get("/api/v1/provinces/"+province.id);
+
+        logger.info(result.body);
+
+        expect(result.status).toBe(200);
+        expect(result.body.data.id).toBe(province.id);
+        expect(result.body.data.name).toBe(province.name);
     });
 });
