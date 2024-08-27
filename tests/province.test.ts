615200cb653c706c0710b47f8e4e3a861aa45a4b
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-08-27 23:19:24 +0700

    test/province: test success update & delete.

--- tests/province.test.ts
+++ tests/province.test.ts
@@ -1,7 +1,7 @@
 import { server } from "../src/applications/server";
 import supertest from "supertest";
 import { logger } from "../src/applications/logging";
-import {removeTestProvince, createTestProvince} from "./utils/province-utills";
+import {removeTestProvince, createTestProvince,getTestProvince} from "./utils/province-utills";
 
 
 describe.skip("POST /api/v1/provinces", function(){
@@ -21,7 +21,7 @@ describe.skip("POST /api/v1/provinces", function(){
     });
 });
 
-describe("GET /api/v1/provinces/:id", function(){
+describe.skip("GET /api/v1/provinces/:id", function(){
     let province:any=null;
 
     beforeEach(async() => {
@@ -40,5 +40,58 @@ describe("GET /api/v1/provinces/:id", function(){
         expect(result.status).toBe(200);
         expect(result.body.data.id).toBe(province.id);
         expect(result.body.data.name).toBe(province.name);
+    });
+});
+
+describe.skip("PUT /api/v1/provinces/:id", function(){
+    let province:any=null;
+
+    beforeEach(async() => {
+        province=await createTestProvince();
+    });
+
+    afterEach(async () => {
+       await  removeTestProvince();
+    });
+
+    it("should can get province", async() => {
+        const result=await supertest(server)
+                           .put("/api/v1/provinces/"+province.id)
+                           .send({
+                                name:"Province Update"
+                           });
+
+        logger.info(result.body);
+
+        expect(result.status).toBe(200);
+        expect(result.body.data.id).toBe(province.id);
+        expect(result.body.data.name).toBe("Province Update");
+    });
+});
+
+describe("DELETE /api/v1/provinces/:id", function(){
+    let province:any=null;
+
+    beforeEach(async() => {
+        province=await createTestProvince();
+    });
+
+    afterEach(async () => {
+       await removeTestProvince();
+    });
+
+    it("should can delete province", async() => {
+        let testProvince = await getTestProvince();
+
+        const result=await supertest(server)
+                           .delete("/api/v1/provinces/"+testProvince.id);
+
+        logger.info(result.body);
+
+        expect(result.status).toBe(200);
+        expect(result.body.message).toBe("Delete Success");
+
+        testProvince = await getTestProvince();
+        expect(testProvince).toBeNull();
     });
 });
