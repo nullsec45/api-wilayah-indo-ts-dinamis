7b25af9b13641585d78e001c082d92e70e7a0602
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-08-27 23:18:00 +0700

    feat/province : update & delete service.

--- src/services/ProvinceService.ts
+++ src/services/ProvinceService.ts
@@ -3,7 +3,8 @@ import { prismaClient } from "../applications/database";
 import Validation from "../validations/Validation";
 import { 
     createProvinceValidation, 
-    getProvinceValidation 
+    getProvinceValidation,
+    updateProvinceValidation 
 
 } from "../validations/province-validation";
 import ResponseError from "../errors/ResponseError";
@@ -39,6 +40,53 @@ class ProvinceService{
         }
 
         return province;
+    }
+
+    static async update(id:number, request:Request){
+        const province=Validation.validate(updateProvinceValidation, request);
+
+        const checkContact = await prismaClient.province.count({
+            where: {
+                id
+            },
+        });
+
+        if (checkContact == 0) {
+            throw new ResponseError(404, "Contact is not found!");
+        }
+
+        return prismaClient.province.update({
+            where:{
+                id
+            },
+            data:{
+                name:province.name
+            },
+            select:{
+                id:true,
+                name:true
+            }
+        });
+    }
+
+    static async remove(id:number){
+       const provinceId =Validation.validate(getProvinceValidation, id);
+
+        const checkContact = await prismaClient.province.count({
+            where: {
+                id:provinceId,
+            },
+        });
+
+        if (checkContact == 0) {
+            throw new ResponseError(404, "contact is not found");
+        }
+
+        return prismaClient.province.delete({
+            where: {
+              id: provinceId,
+            },
+        });
     }
 }
 
