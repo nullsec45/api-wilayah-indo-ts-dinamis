4b043edb4f81578e066c2a9168c5b45dbd3a7781
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-08-27 23:18:19 +0700

    feat : update province validation

--- src/validations/province-validation.ts
+++ src/validations/province-validation.ts
@@ -1,12 +1,17 @@
 import Joi from "joi";
 
 const createProvinceValidation=Joi.object({
-    name:Joi.string().min(3)
+    name:Joi.string().required().min(3)
 });
 
 const getProvinceValidation=Joi.number().min(1).positive().required();
 
+const updateProvinceValidation=Joi.object({
+    name:Joi.string().required().min(3)
+});
+
 export {
     createProvinceValidation,
-    getProvinceValidation
+    getProvinceValidation,
+    updateProvinceValidation
 }
