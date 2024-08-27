3524c0cdf661c462eca2449b9a7f6cbcde091f8d
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-08-27 23:17:31 +0700

    feat/province : update & delete routes.

--- src/routes/ProvinceRoute.ts
+++ src/routes/ProvinceRoute.ts
@@ -6,6 +6,8 @@ class ProvinceRoutes extends BaseRoutes{
     public routes():void{
         this.router.post("/", ProvinceController.create);
         this.router.get("/:id", ProvinceController.show);
+        this.router.put("/:id", ProvinceController.update);
+        this.router.delete("/:id", ProvinceController.delete);
     }
 }
 
