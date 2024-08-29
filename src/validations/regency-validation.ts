0b3d60aa3d57c153b8f844471415dce912e7478d
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-08-29 22:52:02 +0700

    feat/regency: new regency validation

---
+++ src/validations/regency-validation.ts
@@ -0,0 +1,19 @@
+import Joi from "joi";
+
+const createRegencyValidation=Joi.object({
+    name:Joi.string().required().min(3),
+    province_id:Joi.number().min(1).positive().required()
+});
+
+const getRegencyValidation=Joi.number().min(1).positive().required();
+
+const updateRegencyValidation=Joi.object({
+    name:Joi.string().required().min(3),
+    province_id:Joi.number().min(1).positive().required()
+});
+
+export {
+    createRegencyValidation,
+    getRegencyValidation,
+    updateRegencyValidation
+}
