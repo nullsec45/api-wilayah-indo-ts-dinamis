bf767e37c326fd067422f31ddbf4e1a47b8e0e6a
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-08-29 22:50:57 +0700

    feat/regency: register route regency to server

--- src/applications/server.ts
+++ src/applications/server.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { errorMiddleware } from "../middlewares/error-middleware";
-import ProvinceRoute from "../routes/ProvinceRoute";
+import ProvinceRoutes from "../routes/ProvinceRoute";
+import RegencyRoutes from "../routes/RegencyRoute";
 
 export const server=express();
 server.use(express.json());
 
-server.use("/api/v1/provinces", ProvinceRoute);
+server.use("/api/v1/provinces", ProvinceRoutes);
+server.use("/api/v1/regencies", RegencyRoutes);
 server.use(errorMiddleware)
