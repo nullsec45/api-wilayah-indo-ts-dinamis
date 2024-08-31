651e73bc3aaec2116addf14806a458d3e90e59e2
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-08-31 16:54:18 +0700

    feat/regency : shopw, update,delete in controllers.

--- src/controllers/RegencyController.ts
+++ src/controllers/RegencyController.ts
@@ -28,16 +28,45 @@ class RegencyController implements IController{
     }
 
     show= async (req:Request, res:Response, next:NextFunction) => {
-        
+         try{
+            const regencyId=parseInt(req.params.id);
+            const result=await RegencyService.get(regencyId);
+
+            res.status(200).json({
+                data: result,
+            });
+        }catch(error){
+            next(error);
+        }
     }
 
     update= async (req:Request, res:Response, next:NextFunction) => {
-      
+      try{
+            const id=parseInt(req.params.id);
+            const request=req.body;
+            const result=await RegencyService.update(id, request);
+
+            res.status(200).json({
+                message:"Update Success",
+                data: result,
+            });
+        }catch(error){
+            next(error);
+        }
         
     }
 
     delete= async (req:Request, res:Response, next:NextFunction) => {
-      
+         try{
+            const regencyId=parseInt(req.params.id);
+            await RegencyService.remove(regencyId);
+
+            res.status(200).json({
+                 message: "Delete Success",
+            });
+        }catch(error){
+            next(error);
+        }
     }
 }
 
