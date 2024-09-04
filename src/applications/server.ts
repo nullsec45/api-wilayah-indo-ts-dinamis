036c76fff35d9d908b623cda94d22b2c03df3872
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-09-04 23:08:02 +0700

    feat/district : register route district to server

--- src/applications/server.ts
+++ src/applications/server.ts
@@ -2,10 +2,12 @@ import express from "express";
 import { errorMiddleware } from "../middlewares/error-middleware";
 import ProvinceRoutes from "../routes/ProvinceRoute";
 import RegencyRoutes from "../routes/RegencyRoute";
+import DistrictRoutes from "../routes/DistrictRoute";
 
 export const server=express();
 server.use(express.json());
 
 server.use("/api/v1/provinces", ProvinceRoutes);
 server.use("/api/v1/regencies", RegencyRoutes);
+server.use("/api/v1/districts", DistrictRoutes);
 server.use(errorMiddleware)
