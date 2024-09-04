789d710fe55990f5080ee4d190ac469fa30cc645
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-09-04 23:14:45 +0700

    test/district : new utils district.

---
+++ tests/utils/district-utils.ts
@@ -0,0 +1,58 @@
+import {prismaClient} from "../../src/applications/database";
+import {  createTestRegency } from "./regency-utils";
+
+const removeTestDistrict=async () => {
+    await prismaClient.district.deleteMany({});
+    await prismaClient.$executeRaw`ALTER TABLE districts AUTO_INCREMENT = 1`;
+}
+
+const createTestDistrict=async () => {
+   const regency=await createTestRegency();
+   const district= await prismaClient.district.create({
+        data:{
+            name:"Regency Test",
+             regency: {
+               connect: { id: regency.id } 
+           }
+        }
+    })
+
+    return district;
+}
+
+const getTestDistrict=async () => {
+   let district= await prismaClient.district.findFirst({
+        where:{
+            name:"District Test"
+        },
+        select:{
+            id:true,
+            regency_id:true,
+            name:true
+        }
+    })
+
+    return district
+};
+
+const createManyTestDistrict=async () => {
+   const regency=await createTestRegency();
+
+    for(let i=0;i  <  15;i++){
+        await prismaClient.district.create({
+           data:{
+              name:`Regency ${i}`,
+                regency: {
+                  connect: { id: regency.id } 
+                }
+           }
+        });
+    }
+}
+
+export {
+    removeTestDistrict,
+    createTestDistrict,
+    getTestDistrict,
+    createManyTestDistrict,
+}
