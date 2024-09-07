b138fcbe889c6a66610c834eba0f7654b48ff338
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-09-07 13:42:26 +0700

    faet/village : register route to server

--- src/applications/server.ts
+++ src/applications/server.ts
@@ -3,6 +3,7 @@ import { errorMiddleware } from "../middlewares/error-middleware";
 import ProvinceRoutes from "../routes/ProvinceRoute";
 import RegencyRoutes from "../routes/RegencyRoute";
 import DistrictRoutes from "../routes/DistrictRoute";
+import VillageRoutes from "../routes/VillageRoute";
 
 export const server=express();
 server.use(express.json());
@@ -10,4 +11,5 @@ server.use(express.json());
 server.use("/api/v1/provinces", ProvinceRoutes);
 server.use("/api/v1/regencies", RegencyRoutes);
 server.use("/api/v1/districts", DistrictRoutes);
+server.use("/api/v1/villages", VillageRoutes);
 server.use(errorMiddleware)
