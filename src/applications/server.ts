cb6b2cd468d8fe73437142c0f59373beddeb0c3f
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-09-17 14:16:09 +0700

    feat/user : resgister user route to server

--- src/applications/server.ts
+++ src/applications/server.ts
@@ -4,6 +4,7 @@ import ProvinceRoutes from "../routes/ProvinceRoute";
 import RegencyRoutes from "../routes/RegencyRoute";
 import DistrictRoutes from "../routes/DistrictRoute";
 import VillageRoutes from "../routes/VillageRoute";
+import UserRoute from "../routes/UserRoute";
 
 export const server=express();
 server.use(express.json());
@@ -12,4 +13,6 @@ server.use("/api/v1/provinces", ProvinceRoutes);
 server.use("/api/v1/regencies", RegencyRoutes);
 server.use("/api/v1/districts", DistrictRoutes);
 server.use("/api/v1/villages", VillageRoutes);
+
+server.use("/api/v1/users", UserRoute);
 server.use(errorMiddleware)
