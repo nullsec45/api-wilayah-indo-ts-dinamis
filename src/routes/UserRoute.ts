6db8748b984f34108138d3c7f003b22b6d80020d
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-09-17 14:17:38 +0700

    feat/user : new user route.

---
+++ src/routes/UserRoute.ts
@@ -0,0 +1,15 @@
+import BaseRoutes from "./BaseRoutes";
+
+import UserController from "../controllers/UserController";
+import {authMiddleware} from "../middlewares/auth-middleware"
+
+class UserRoutes extends BaseRoutes{
+    public routes():void{
+        this.router.post("/auth/login", UserController.login);
+        this.router.post("/auth/register", UserController.register);
+        this.router.put("/:id", authMiddleware, UserController.update);
+        this.router.delete("/:id", UserController.delete);
+    }
+}
+
+export default new UserRoutes().router;
