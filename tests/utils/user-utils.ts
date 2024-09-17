168215f26b25ba5fa46cda0b7822e4c44b270498
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-09-17 14:18:18 +0700

    test/user: new utils user.

---
+++ tests/utils/user-utils.ts
@@ -0,0 +1,38 @@
+import {prismaClient} from "../../src/applications/database";
+import bcrypt from "bcrypt";
+
+const deleteUser=async () => {
+    await prismaClient.user.deleteMany({});
+    await prismaClient.$executeRaw`ALTER TABLE users AUTO_INCREMENT = 1`;
+}
+
+const createUser=async () => {
+    await prismaClient.user.create({
+            data:{
+                username:"test",
+                name:"test",
+                password:await bcrypt.hash("test",10),
+                token: "test"
+            }
+        });
+}
+
+const getUser=async() => {
+    const user=await prismaClient.user.findFirst({
+        where:{
+            username:"test"
+        }
+    });
+   
+    if(!user){
+        throw new Error("User is not found");
+    }
+
+    return user;
+}
+
+export {
+    deleteUser,
+    createUser,
+    getUser
+}
