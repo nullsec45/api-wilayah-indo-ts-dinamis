18c07c0690db150489949e9f44e005960d0f787d
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-10-23 06:21:52 +0700

    feat/user : implements : model user request & response in controller.

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -3,11 +3,14 @@ import VillageService from "../services/VillageService";
 import AuthController from "./AuthControllerInterface";
 import UserService from "../services/UserService";
 import { logger } from "../applications/logging";
+import { CreateUserRequest } from "../models/user-model";
+import { LoginUserRequest } from "../../models/user-model";
 
 class UserController implements AuthController{
     login= async (req:Request, res:Response, next:NextFunction) => {
        try{
-            const result=await UserService.login(req.body);
+            const request:LoginUserRequest=req.body as LoginUserRequest;
+            const result=await UserService.login(request);
             
             res.status(200).json({
                 data: result,
@@ -19,7 +22,8 @@ class UserController implements AuthController{
 
     register= async (req:Request, res:Response, next:NextFunction) => {
        try{
-            const result=await UserService.register(req.body);
+            const request:CreateUserRequest=req.body as CreateUserRequest;
+            const result=await UserService.register(request);
 
             logger.info(result);
             
